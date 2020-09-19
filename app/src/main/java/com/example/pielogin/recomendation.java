ef95b75d2f97cf6448363b7e78f40623585c8559
cnsangmin <69336374+cnsangmin@users.noreply.github.com>
2020-09-19 14:16:07 +0900

    added new page: movieroom connected - recom & room - room & movie used new way to add functioning invisible button for recomendation.xml idk th is wrong with manifest but idc

--- app/src/main/java/com/example/pielogin/recomendation.java
+++ app/src/main/java/com/example/pielogin/recomendation.java
@@ -2,7 +2,11 @@ package com.example.pielogin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class recomendation extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class recomendation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recomendation);
+
+        Button ch1 = findViewById(R.id.choice1);
+        ch1.setVisibility(View.VISIBLE);
+        ch1.setBackgroundColor(Color.TRANSPARENT);
+
+        ch1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openRoom();
+            }
+        });
+    }
+
+    public void openRoom() {
+        Intent intent = new Intent(this, room.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/pielogin/room.java
+++ app/src/main/java/com/example/pielogin/room.java
@@ -2,27 +2,30 @@ package com.example.pielogin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.animation.AnimationUtils;
 import android.widget.Button;
 
 public class room extends AppCompatActivity {
 
-    Button bleft, shell1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.room1);
 
-        bleft = (Button)findViewById((R.id.butLeft));
-        shell1 = (Button)findViewById(R.id.movieroom);
+        Button movie = findViewById(R.id.movieroom);
 
-        bleft.setOnClickListener(new View.OnClickListener() {
+        movie.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                AnimationUtils.loadAnimation()
+                openMovie();
             }
         });
+    }
+
+    public void openMovie() {
+        Intent intent = new Intent(this, movie.class);
+        startActivity(intent);
     }
 }
