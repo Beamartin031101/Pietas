4daa16eed9f899f6234acd800f6af8fbe037f6db
cnsangmin <69336374+cnsangmin@users.noreply.github.com>
2020-09-19 14:09:20 +0900

    added class for movie room added bucha images needed: - fish bg - coralsand bg added drawable for round buttons

---
+++ app/src/main/java/com/example/pielogin/movie.java
@@ -0,0 +1,15 @@
+package com.example.pielogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class movie extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.movieroom);
+
+    }
+}
