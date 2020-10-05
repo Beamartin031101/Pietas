58dbfd3c377c06ab93eb3d167fe1836d8d69baee
Beamartin031101 <martin03110170@gmail.com>
2020-10-06 08:55:24 +0900

    페이스북 로그인 기능 추가

--- app/src/main/java/com/example/pielogin/login.java
+++ app/src/main/java/com/example/pielogin/login.java
@@ -11,6 +11,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -21,9 +27,13 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
+
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 
 public class login extends AppCompatActivity {
 
@@ -31,17 +41,11 @@ public class login extends AppCompatActivity {
     private FirebaseAuth mAuth = null;
     private GoogleSignInClient mGoogleSignInClient;
     private static final int RC_SIGN_IN = 9001;
+    private static final int FB_SIGN_IN = 64206;
     private static final String TAG = "LoginActivity";
 
-    //Check User sign in
-    @Override
-    protected void onStart() {
-        super.onStart();
-        // Check If user is signed in -> update UI
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        //updateUI(currentUser); //TODO: 이게 뭐하는 놈인지 알아보기
-        //이 친구는 구글 공식 문서에서는 signInBtn을 invisible로 바꾸고 signOutBtn을 Visible로 바꾸는 친구임
-    }
+    //facebook Variable init
+    private CallbackManager mCallbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,6 +73,28 @@ public class login extends AppCompatActivity {
             }
         });
 
+        //Init facebook Login Button
+        mCallbackManager = CallbackManager.Factory.create();
+        LoginButton FLoginButton = findViewById(R.id.fbutton);
+        FLoginButton.setReadPermissions("email", "public_profile");
+        FLoginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(TAG, "Facebook - onSuccess" + loginResult);
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG,"Facebook - onCancel");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(TAG, "Facebook - onError" + error);
+            }
+        });
+
         //Please write explanation
         Button button = findViewById(R.id.confirm);
         button.setOnClickListener(new View.OnClickListener() {
@@ -77,6 +103,16 @@ public class login extends AppCompatActivity {
                 openRecom();
             }
         });
+    }
+
+    //Check User sign in
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Check If user is signed in -> update UI
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser); //TODO: 이게 뭐하는 놈인지 알아보기
+        //이 친구는 구글 공식 문서에서는 signInBtn을 invisible로 바꾸고 signOutBtn을 Visible로 바꾸는 친구임
     }
 
     //다음 페이지인 Recommendation으로 가는 부분
@@ -97,19 +133,25 @@ public class login extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
         // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            try {
-                // Google Sign In was successful, authenticate with Firebase
-                GoogleSignInAccount account = task.getResult(ApiException.class);
-                assert account != null; //만약 Account = Null이면 에러를 발생시킴
-                //TODO: 이거를 나중에 assert에서 로그인 실패로 바꾸기
-                Log.d(TAG, "firebaseAuthWithGoogle:" + account.getId());
-                firebaseAuthWithGoogle(account.getIdToken());
-            } catch (ApiException e) {
-                // Google Sign In failed, update UI appropriately
-                Log.w(TAG, "Google sign in failed", e);
-                // ...
+        switch(requestCode){
+            case RC_SIGN_IN:{
+                Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+                try {
+                    // Google Sign In was successful, authenticate with Firebase
+                    GoogleSignInAccount account = task.getResult(ApiException.class);
+                    assert account != null; //만약 Account = Null이면 에러를 발생시킴
+                    //TODO: 이거를 나중에 assert에서 로그인 실패로 바꾸기
+                    Log.d(TAG, "firebaseAuthWithGoogle:" + account.getId());
+                    firebaseAuthWithGoogle(account.getIdToken());
+                } catch (ApiException e) {
+                    // Google Sign In failed, update UI appropriately
+                    Log.w(TAG, "Google sign in failed", e);
+                    // ...
+                }
+            }
+            case FB_SIGN_IN:{
+                mCallbackManager.onActivityResult(requestCode, resultCode, data);
+                // DANGER - 이거 그냥 내가 실험으로 FB_SIGN_IN 구한 거라서 에러의 가능성이 있음
             }
         }
     }
@@ -131,6 +173,30 @@ public class login extends AppCompatActivity {
                             // If sign in fails, display a message to the user.
                             Log.w(TAG, "signInWithCredential:failure", task.getException());
                             Toast.makeText(login.this, "로그인 실패!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    //Upload userdata to firebase
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.d(TAG, "handleFacebookAccessToken:" + token);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            openRecom();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(login.this, "Login failed.",
+                                    Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
